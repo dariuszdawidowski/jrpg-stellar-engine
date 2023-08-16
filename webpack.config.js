949156f4e7c510e01c723e89260787931bffb50b
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2023-08-16 21:54:45 +0200

    Work on webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: {
-        bundle: ['./src/scripts/engine.js', './src/scripts/player.js', './src/scripts/sprite.js']
+        'bundle.js': [
+            path.resolve(__dirname, 'src/scripts/engine.js'),
+            path.resolve(__dirname, 'src/scripts/player.js'),
+            path.resolve(__dirname, 'src/scripts/sprite.js')
+        ]
     },
     output: {
-        filename: 'bundle.js',
+        filename: '[name]',
         path: path.resolve(__dirname, 'public')
     },
     plugins: [
