1c5ff357c79667bf40047e10ff48324a302b9cc6
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-03-27 20:00:41 +0100

    Separate player to own script with scroll handling.

--- src/player.js
+++ src/player.js
@@ -1,63 +1,16 @@
-class Sprite extends TileSet {
+class Player extends Sprite {
 
     /**
      * Create sprite
-     * All TileSet params plus:
-     * @param speed: Number - movement speed (pixels on second)
-     * @param anim: Object - map of animations { speed: seconds, idle: [], moveUp: [], moveDown: [], moveLeft: [], moveRight: [] }
+     * Sll TileSet+Sprite params plus:
+     * @param scroll: Object - range for scroll { top: Number, bottom: Number, left: Number, right: Number }
      */
 
     constructor(args) {
         super(args);
 
-        // Stats
-        this.stats = {
-            speed: args.speed,
-        };
-
-        /**
-         * Position in space
-         * v: vertical action (n=north,s=south)
-         * h: horizontal action (w=west,e=east)
-         * x,y: screen
-         */
-        this.transform = {
-            v: '',
-            h: '',
-            x: 0,
-            y: 0,
-            // wx: 0,
-            // wy: 0,
-        };
-
-        // Animation map
-        this.anim = 'anim' in args ? args.anim : { speed: 0, idle: [], moveUp: [], moveDown: [], moveLeft: [], moveRight: [] };
-        this.anim.speed = this.anim.speed / 100.0;
-
-        // Current frame
-        this.frame = 0;
-
-        // Current frame counter (horizontal movement)
-        this.frameCounterH = 0;
-
-        // Time of the last animation frame (horizontal movement)
-        this.frameTimeH = this.anim.speed;
-
-        // Current frame counter (vertical movement)
-        this.frameCounterV = 0;
-
-        // Time of the last animation frame (vertical movement)
-        this.frameTimeV = this.anim.speed;
-    }
-
-    /**
-     * Idle movement
-     */
-
-    idle() {
-        this.frame = this.anim.idle[0];
-        this.transform.v = '';
-        this.transform.h = '';
+        // Scroll range
+        this.scroll = 'scroll' in args ? args.scroll : null;
     }
 
     /**
@@ -80,7 +33,12 @@ class Sprite extends TileSet {
 
         // Move
         const pixels = this.stats.speed * deltaTime;
-        this.transform.y -= pixels;
+        // this.transform.wy -= pixels;
+        if (this.scroll && this.transform.y > this.scroll.top) {
+            this.transform.y -= pixels;
+            return 0;
+        }
+
         return pixels;
     }
 
@@ -104,7 +62,12 @@ class Sprite extends TileSet {
 
         // Move
         const pixels = this.stats.speed * deltaTime;
-        this.transform.y += pixels;
+        // this.transform.wy += pixels;
+        if (this.scroll && this.transform.y < this.scroll.bottom) {
+            this.transform.y += pixels;
+            return 0;
+        }
+
         return pixels;
     }
 
@@ -128,7 +91,12 @@ class Sprite extends TileSet {
 
         // Move
         const pixels = this.stats.speed * deltaTime
-        this.transform.x += pixels;
+        // this.transform.wx += pixels;
+        if (this.scroll && this.transform.x < this.scroll.right) {
+            this.transform.x += pixels;
+            return 0;
+        }
+
         return pixels;
     }
 
@@ -152,16 +120,13 @@ class Sprite extends TileSet {
 
         // Move
         const pixels = this.stats.speed * deltaTime;
-        this.transform.x -= pixels;
+        // this.transform.wx -= pixels;
+        if (this.scroll && this.transform.x > this.scroll.left) {
+            this.transform.x -= pixels;
+            return 0;
+        }
+
         return pixels;
-    }
-
-    /**
-     * Render sprite
-     */
-
-    render() {
-        super.renderSingle({x: this.transform.x, y: this.transform.y, nr: this.frame});
     }
 
 }
