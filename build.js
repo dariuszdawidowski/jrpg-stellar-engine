4b695a2e5385f89fa77683dd4a336c4c663c59ab
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-04-09 22:59:21 +0200

    New build system

---
+++ build.js
@@ -0,0 +1,33 @@
+/**
+ * Build script v6-custom
+ */
+
+const files = [
+    { src: 'stellar-engine.js.ejs', dst: 'stellar-engine.js' },
+];
+
+const fs = require('fs');
+const { readFile } = require('fs').promises;
+const ejs = require('ejs');
+const { minify } = require('terser');
+
+const minjs = async (filePath) => {
+    try {
+        const inputCode = await readFile(filePath, 'utf8');
+        const minifiedCode = (await minify(inputCode)).code;
+        return minifiedCode;
+    }
+    catch (error) {
+        console.error(`Error minifying js ${filePath}:`, error);
+        return null;
+    }
+};
+
+if (!fs.existsSync('dist')) fs.mkdirSync('dist');
+
+files.forEach((file) => {
+
+    ejs.render(fs.readFileSync(file.src, 'utf8'), { minjs }, {async: true})
+        .then(output => fs.writeFileSync('dist/' + file.dst, output, 'utf8'));
+
+});
