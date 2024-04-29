644f6146d193b4bbff24c5c80e47166538b76925
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-04-29 12:32:32 +0200

    Added sources for level and test animation.;

---
+++ level/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="test" tilewidth="16" tileheight="16" tilecount="69" columns="20">
+ <image source="../public/sprites/environment/ground.png" width="320" height="48"/>
+</tileset>
---
+++ level/water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="water" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="../public/sprites/environment/water.png" width="48" height="48"/>
+</tileset>
