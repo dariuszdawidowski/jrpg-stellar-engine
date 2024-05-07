8cb70f75c337a4b64d5fb49457e5342b924e258e
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-05-07 15:44:04 +0200

    Added cave.

--- level/test.tsx
+++ level/test.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="test" tilewidth="16" tileheight="16" tilecount="69" columns="20">
- <image source="../public/sprites/environment/ground.png" width="320" height="48"/>
+<tileset version="1.10" tiledversion="1.10.2" name="test" tilewidth="16" tileheight="16" tilecount="87" columns="29">
+ <image source="../public/sprites/environment/ground.png" width="464" height="48"/>
 </tileset>
